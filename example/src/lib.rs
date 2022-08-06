5f03adefcf77203b7281d7027b7f7476116541ff
Craig <CraigM@CraigM.info>
2022-08-06 11:47:51 -0700

    setup workspace with example

---
+++ example/src/lib.rs
@@ -0,0 +1,22 @@
+#![cfg(target_arch = "wasm32")]
+
+extern crate alloc;
+
+use lol_alloc::SimpleAllocator;
+
+#[global_allocator]
+static ALLOCATOR: SimpleAllocator = SimpleAllocator::new();
+
+use alloc::boxed::Box;
+
+// Box a `u8`!
+#[no_mangle]
+pub extern "C" fn hello() -> *mut u8 {
+    Box::into_raw(Box::new(42))
+}
+
+// Free a `Box<u8>` that we allocated earlier!
+#[no_mangle]
+pub unsafe extern "C" fn goodbye(ptr: *mut u8) {
+    let _ = Box::from_raw(ptr);
+}
